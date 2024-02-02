582334874171c1595685b4dd8433d423e5b88cbe
Quick Chang <quick0831@gmail.com>
2024-02-02 21:03:01 +0800

    refactor to using framework

---
+++ src/command.rs
@@ -0,0 +1,47 @@
+use serenity::prelude::*;
+use serenity::all::Message;
+use serenity::framework::standard::macros::group;
+use serenity::framework::standard::macros::command;
+use serenity::framework::standard::CommandResult;
+
+#[group]
+#[commands(ping, join, leave)]
+struct General;
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "Pong!").await?;
+    Ok(())
+}
+
+#[command]
+#[only_in(guilds)]
+async fn join(ctx: &Context, msg: &Message) -> CommandResult {
+    let manager = songbird::get(&ctx).await.expect("Songbird Not initialized");
+    let channel_id = msg.guild(&ctx.cache).unwrap()
+        .voice_states.get(&msg.author.id)
+        .and_then(|state| state.channel_id);
+    let return_msg = if let Some(c) = channel_id {
+        match manager.join(msg.guild_id.unwrap(), c).await {
+            Ok(_) => "JOIN!".to_owned(),
+            Err(e) => format!("Join failed: {e:?}")
+        }
+    } else {
+        "Not in a voice channel".to_owned()
+    };
+    
+    msg.channel_id.say(&ctx.http, return_msg).await?;
+    Ok(())
+}
+
+#[command]
+#[only_in(guilds)]
+async fn leave(ctx: &Context, msg: &Message) -> CommandResult {
+    let manager = songbird::get(&ctx).await.expect("Songbird Not initialized");
+    let return_msg = match manager.leave(msg.guild_id.unwrap()).await {
+        Ok(_) => "LEAVE!".to_owned(),
+        Err(e) => format!("Leave failed: {e:?}")
+    };
+    msg.channel_id.say(&ctx.http, return_msg).await?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-use serenity::all::Message;
 use serenity::all::Ready;
 use serenity::async_trait;
+use serenity::framework::StandardFramework;
+use serenity::framework::standard::Configuration;
 use serenity::prelude::*;
 use serenity::Client;
 use serenity::client::EventHandler;
 
 use songbird::SerenityInit;
-use tracing::error;
 use tracing::info;
 use tracing::instrument;
 use tracing_subscriber;
+
+mod command;
+use command::GENERAL_GROUP;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -19,8 +22,11 @@ async fn main() -> anyhow::Result<()> {
     let token = dotenvy::var("DISCORD_TOKEN")?;
     let intents = GatewayIntents::non_privileged()
         | GatewayIntents::MESSAGE_CONTENT;
+    let framework = StandardFramework::new().group(&GENERAL_GROUP);
+    framework.configure(Configuration::new().prefix("]"));
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
+        .framework(framework)
         .register_songbird()
         .await?;
     client.start().await?;
@@ -35,41 +41,5 @@ impl EventHandler for Handler {
     #[instrument(skip_all)]
     async fn ready(&self, _ctx: Context, ready: Ready) {
         info!("{} is Ready!", ready.user.name);
-    }
-
-    #[instrument(skip_all, fields(msg.content))]
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.author.bot { return; }
-        if msg.channel_id.get() != 1202687853424017458 { return; }
-        
-        if matches!(msg.content.get(..1), Some("]") | Some("}")) {
-            let s = msg.content[1..].split(" ").collect::<Vec<_>>();
-            let cmd = s[0].to_lowercase();
-            let ret_msg = if matches!(cmd.as_str(), "j" | "join") {
-                let manager = songbird::get(&ctx).await.expect("Songbird Not initialized");
-                let channel_id = msg.guild(&ctx.cache).unwrap()
-                    .voice_states.get(&msg.author.id)
-                    .and_then(|state| state.channel_id);
-                if let Some(c) = channel_id {
-                    match manager.join(msg.guild_id.unwrap(), c).await {
-                        Ok(_) => "JOIN!".to_owned(),
-                        Err(e) => format!("Join failed: {e:?}")
-                    }
-                } else {
-                    "Not in a voice channel".to_owned()
-                }
-            } else if matches!(cmd.as_str(), "l" | "leave") {
-                let manager = songbird::get(&ctx).await.expect("Songbird Not initialized");
-                match manager.leave(msg.guild_id.unwrap()).await {
-                    Ok(_) => "LEAVE!".to_owned(),
-                    Err(e) => format!("Leave failed: {e:?}")
-                }
-            } else {
-                format!("Command received: {}\nDebug: {:?}", s[0], s)
-            };
-            if let Err(e) = msg.channel_id.say(&ctx.http, ret_msg).await {
-                error!("{e}");
-            }
-        }
     }
 }
