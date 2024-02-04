cd46a2c5cb6c2fe460960010fb43c0ffc1081eaa
Quick Chang <quick0831@gmail.com>
2024-02-04 15:26:36 +0800

    refactor to using `poise` as framework

--- src/command.rs
+++ src/command.rs
@@ -1,60 +1,53 @@
-use std::collections::HashSet;
-
-use serenity::all::Message;
-use serenity::all::UserId;
-use serenity::framework::standard::{
-    Args,
-    CommandGroup,
-    CommandResult,
-    HelpOptions,
-    help_commands,
-    macros::command,
-    macros::group,
-    macros::help,
-};
-use serenity::prelude::*;
 use songbird::input::YoutubeDl;
 
+use poise::command;
+
+use crate::Context;
 use crate::get_client;
 
-#[help]
-async fn help_command(
-    context: &Context,
-    msg: &Message,
-    args: Args,
-    help_options: &'static HelpOptions,
-    groups: &[&'static CommandGroup],
-    owners: HashSet<UserId>,
-) -> CommandResult {
-    let _ = help_commands::with_embeds(context, msg, args, help_options, groups, owners).await;
+/// Show this help menu
+#[command(prefix_command, track_edits, slash_command)]
+pub async fn help(
+    ctx: Context<'_>,
+    #[description = "Specific command to show help about"]
+    #[autocomplete = "poise::builtins::autocomplete_command"]
+    command: Option<String>,
+) -> anyhow::Result<()> {
+    poise::builtins::help(
+        ctx,
+        command.as_deref(),
+        poise::builtins::HelpConfiguration {
+            extra_text_at_bottom: "This is an example bot made to showcase features of my custom Discord bot framework",
+            ..Default::default()
+        },
+    )
+    .await?;
     Ok(())
 }
 
-#[group]
-#[commands(ping, join, leave, play)]
-struct General;
-
-#[command]
-#[description = "As typical as it was"]
-async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.channel_id.say(&ctx.http, "Pong!").await?;
+/// As typical as it was
+#[command(prefix_command)]
+pub async fn ping(
+    ctx: Context<'_>,
+) -> anyhow::Result<()> {
+    ctx.say("Pong!").await?;
     Ok(())
 }
 
-#[command]
-#[only_in(guilds)]
-#[aliases(j)]
-#[description = "Join the voice channel you are in"]
-async fn join(ctx: &Context, msg: &Message) -> CommandResult {
-    let manager = songbird::get(&ctx).await.expect("Songbird Not initialized");
-    let channel_id = msg
-        .guild(&ctx.cache)
+/// Join the voice channel you are in
+#[command(prefix_command, guild_only)]
+pub async fn join(
+    ctx: Context<'_>,
+) -> anyhow::Result<()> {
+    let manager = songbird::get(&ctx.serenity_context()).await.expect("Songbird Not initialized");
+    let channel_id = ctx
+        .guild()
         .unwrap()
         .voice_states
-        .get(&msg.author.id)
+        .get(&ctx.author().id)
         .and_then(|state| state.channel_id);
     let return_msg = if let Some(c) = channel_id {
-        match manager.join(msg.guild_id.unwrap(), c).await {
+        match manager.join(ctx.guild_id().unwrap(), c).await {
             Ok(_) => "JOIN!".to_owned(),
             Err(e) => format!("Join failed: {e:?}"),
         }
@@ -62,35 +55,35 @@ async fn join(ctx: &Context, msg: &Message) -> CommandResult {
         "Not in a voice channel".to_owned()
     };
 
-    msg.channel_id.say(&ctx.http, return_msg).await?;
+    ctx.say(return_msg).await?;
     Ok(())
 }
 
-#[command]
-#[only_in(guilds)]
-#[aliases(l)]
-#[description = "Leave the voice channel"]
-async fn leave(ctx: &Context, msg: &Message) -> CommandResult {
-    let manager = songbird::get(&ctx).await.expect("Songbird Not initialized");
-    let return_msg = match manager.leave(msg.guild_id.unwrap()).await {
+/// Leave the voice channel
+#[command(prefix_command, guild_only)]
+pub async fn leave(
+    ctx: Context<'_>,
+) -> anyhow::Result<()> {
+    let manager = songbird::get(&ctx.serenity_context()).await.expect("Songbird Not initialized");
+    let return_msg = match manager.leave(ctx.guild_id().unwrap()).await {
         Ok(_) => "LEAVE!".to_owned(),
         Err(e) => format!("Leave failed: {e:?}"),
     };
-    msg.channel_id.say(&ctx.http, return_msg).await?;
+    ctx.say(return_msg).await?;
     Ok(())
 }
 
-#[command]
-#[only_in(guilds)]
-#[aliases(p)]
-#[description = "Play a song"]
-async fn play(ctx: &Context, msg: &Message) -> CommandResult {
-    let manager = songbird::get(&ctx).await.expect("Songbird Not initialized");
+/// Play a song
+#[command(prefix_command, guild_only)]
+pub async fn play(
+    ctx: Context<'_>,
+) -> anyhow::Result<()> {
+    let manager = songbird::get(&ctx.serenity_context()).await.expect("Songbird Not initialized");
     let client = get_client().await;
     let url = "https://www.youtube.com/watch?v=i8OUh3YvRpk".to_string();
     let f = YoutubeDl::new(client, url);
-    let call = manager.get_or_insert(msg.guild_id.expect("Command in Guild Channel"));
+    let call = manager.get_or_insert(ctx.guild_id().unwrap());
     (*call).lock().await.play(f.into());
-    msg.channel_id.say(&ctx.http, "Play!").await?;
+    ctx.say("Play!").await?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
-use command::HELP_COMMAND;
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::time::Duration;
+
 use serenity::all::Ready;
 use serenity::async_trait;
 use serenity::client::EventHandler;
-use serenity::framework::standard::Configuration;
-use serenity::framework::StandardFramework;
 use serenity::prelude::*;
 use serenity::Client;
 
 use songbird::SerenityInit;
+
+use tracing::error;
 use tracing::info;
 use tracing::instrument;
 use tracing_subscriber;
 
 mod command;
-use command::GENERAL_GROUP;
 
 static CLIENT: RwLock<Option<reqwest::Client>> = RwLock::const_new(None);
+
+pub struct Data {
+    votes: Mutex<HashMap<String, u32>>,
+}
+type Context<'a> = poise::Context<'a, Data, anyhow::Error>;
 
 async fn get_client() -> reqwest::Client {
     {
@@ -29,19 +36,76 @@ async fn get_client() -> reqwest::Client {
     client
 }
 
+async fn on_error(error: poise::FrameworkError<'_, Data, anyhow::Error>) {
+    match error {
+        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Command { error, ctx, .. } => {
+            error!("Error in command `{}`: {:?}", ctx.command().name, error);
+        }
+        error => {
+            if let Err(e) = poise::builtins::on_error(error).await {
+                error!("Error while handling error: {}", e)
+            }
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let subscriber = tracing_subscriber::FmtSubscriber::new();
     tracing::subscriber::set_global_default(subscriber)?;
 
     let token = dotenvy::var("DISCORD_TOKEN")?;
+
+    let options = poise::FrameworkOptions {
+        commands: vec![
+            command::help(),
+            command::ping(),
+            command::join(),
+            command::leave(),
+            command::play(),
+        ],
+        prefix_options: poise::PrefixFrameworkOptions {
+            prefix: Some("]".into()),
+            additional_prefixes: vec![
+                poise::Prefix::Literal("}"),
+            ],
+            edit_tracker: Some(Arc::new(poise::EditTracker::for_timespan(
+                Duration::from_secs(3600),
+            ))),
+            ..Default::default()
+        },
+        on_error: |err| Box::pin(on_error(err)),
+        pre_command: |ctx| {
+            Box::pin(async move {
+                info!("Executing command {}...", ctx.command().qualified_name);
+            })
+        },
+        event_handler: |_ctx, event, _framework, _data| {
+            Box::pin(async move {
+                info!(
+                    "Got an event in event handler: {:?}",
+                    event.snake_case_name()
+                );
+                Ok(())
+            })
+        },
+        ..Default::default()
+    };
+    let framework = poise::Framework::builder()
+        .setup(move |ctx, ready, framework| {
+            Box::pin(async move {
+                info!("Logged in as {}", ready.user.name);
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {
+                    votes: Mutex::new(HashMap::new()),
+                })
+            })
+        })
+        .options(options)
+        .build();
+
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
-    let framework = StandardFramework::new().group(&GENERAL_GROUP).help(&HELP_COMMAND);
-    framework.configure(
-        Configuration::new()
-            .prefixes(["]", "}"])
-            .case_insensitivity(true),
-    );
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
         .framework(framework)
@@ -57,7 +121,7 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     #[instrument(skip_all)]
-    async fn ready(&self, _ctx: Context, ready: Ready) {
+    async fn ready(&self, _ctx: serenity::prelude::Context, ready: Ready) {
         info!("{} is Ready!", ready.user.name);
     }
 }
